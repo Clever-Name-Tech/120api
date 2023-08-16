49b9a606696f41d8e6007010b1879b1a60797988
Michael <59299019+csnewb@users.noreply.github.com>
2023-08-15 20:32:17 -0700

    update testing url route

--- app.py
+++ app.py
@@ -74,5 +74,23 @@ def update_url():
 
     return f"POST URL set to: {post_url_path}"
 
+@app.route('/update-testing_url', methods=['POST'])
+def update_testing_url():
+    new_url = request.form.get('URL')
+    if not new_url:
+        return "URL parameter is required.", 400
+
+    # Validate the URL (you may want more robust validation depending on your use case)
+    if not new_url.startswith("http://") and not new_url.startswith("https://"):
+        return "Invalid URL.", 400
+
+    post_url_path = 'TEST_URL.py'
+
+    # Write the new URL value back to the file
+    with open(post_url_path, 'w') as file:
+        file.write(f"URL = \"{new_url}\"\n")
+
+    return f"TEST URL set to: {post_url_path}"
+
 if __name__ == "__main__":
     app.run()
